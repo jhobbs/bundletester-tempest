fc510a9381879cac9b093651dab17c0f3b6afb1b
Jason Hobbs <jason.hobbs@gmail.com>
2016-06-07 13:55:43 -0500

    Fix comment error.

--- run.sh
+++ run.sh
@@ -15,4 +15,4 @@ pip install bundletester --upgrade
 
 juju_env=$1
 
-#bundletester -v -l DEBUG -e $juju_env --no-destroy -r json -F -x keystone
+bundletester -v -l DEBUG -e $juju_env --no-destroy -r json -F -x keystone
